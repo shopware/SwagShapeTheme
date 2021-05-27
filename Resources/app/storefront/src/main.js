21bcc4ec004968201cc8fa2ada6f5d97df950325
Tuan Ngo <t.ngo@shopware.com>
2021-05-27 10:06:47 +0700

    TSP-113 - Filter component

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,6 +1,7 @@
 import NavigationMenuPlugin from './plugin/main-menu/navigation-menu.plugin';
 import FlyoutMenuPlugin from './plugin/main-menu/flyout-menu.plugin';
 import ShapeGallerySliderPlugin from './plugin/slider/gallery-slider.plugin';
+import ShapeListingPlugin from './plugin/listing/listing.plugin';
 
 const PluginManager = window.PluginManager;
 
@@ -9,4 +10,5 @@ PluginManager.deregister('SearchWidget', '[data-search-form]');
 PluginManager.register('ShapeNavigationMenu', NavigationMenuPlugin, '[data-navigation-menu]');
 PluginManager.register('ShapeFlyoutMenu', FlyoutMenuPlugin, '[data-navigation-link]');
 
+PluginManager.override('Listing', ShapeListingPlugin, '[data-listing]');
 PluginManager.override('GallerySlider', ShapeGallerySliderPlugin, '[data-gallery-slider]');
