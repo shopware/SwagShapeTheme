c2d6d6304100e8d9ed6dfc9a2734ef3056a80fea
Thu Vo <thu.vo@shopware.com>
2021-05-28 16:55:09 +0700

    TSP-145 - Slider component

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,5 +1,6 @@
 import NavigationMenuPlugin from './plugin/main-menu/navigation-menu.plugin';
 import FlyoutMenuPlugin from './plugin/main-menu/flyout-menu.plugin';
+import ShapeGallerySliderPlugin from './plugin/slider/gallery-slider.plugin';
 
 const PluginManager = window.PluginManager;
 
@@ -7,3 +8,5 @@ PluginManager.deregister('FlyoutMenu', '[data-flyout-menu]');
 PluginManager.deregister('SearchWidget', '[data-search-form]');
 PluginManager.register('ShapeNavigationMenu', NavigationMenuPlugin, '[data-navigation-menu]');
 PluginManager.register('ShapeFlyoutMenu', FlyoutMenuPlugin, '[data-navigation-link]');
+
+PluginManager.override('GallerySlider', ShapeGallerySliderPlugin, '[data-gallery-slider]');
