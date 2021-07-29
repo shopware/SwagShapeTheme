a3641e64a7e672408139160a318c956d6c90a198
Dieu Do <d.do@shopware.com>
2021-07-29 14:23:19 +0700

    TSP-237 - Percy.io integration

---
+++ Resources/app/storefront/test/e2e/cypress/support/index.js
@@ -0,0 +1,27 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+require('@shopware-ag/e2e-testsuite-platform/cypress/support');
+require('@percy/cypress');
+
+require('./commands');
+
+beforeEach(() => {
+    return cy.log('Cleaning, please wait a little bit.').then(() => {
+        return cy.cleanUpPreviousState();
+    }).then(() => {
+        return cy.clearCacheAdminApi('DELETE', `api/_action/cache`);
+    })
+});
