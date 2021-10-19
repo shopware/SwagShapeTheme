a1383e298820be5e9b31f663ef3d832a29e742ef
Long Tran <l.tran@shopware.com>
2021-10-19 14:36:57 +0700

    TSP-265 - Visual testing for color concept

---
+++ Resources/app/storefront/test/e2e/cypress/support/pages/theme.page-object.js
@@ -0,0 +1,32 @@
+function changeColorScheme(colorScheme) {
+    cy.intercept({
+        path: `${Cypress.env('apiPath')}/_action/theme/*`,
+        method: 'patch'
+    }).as('saveData');
+
+    cy.get('.sw-theme-list-item')
+        .last()
+        .get('.sw-theme-list-item__title')
+        .contains('SwagShapeTheme')
+        .click();
+
+    cy.get('.sw-colorpicker .sw-colorpicker__input')
+        .first().clear()
+        .typeAndCheck(colorScheme.primary);
+
+    cy.get('.sw-card__title').contains('E-Commerce')
+        .parent('.sw-theme-manager-detail__area')
+        .find('.sw-colorpicker__input')
+        .first().clear().typeAndCheck(colorScheme.primary);
+
+    cy.get('.smart-bar__actions .sw-button-process.sw-button--primary').click();
+    cy.get('.sw-modal .sw-button--primary').click();
+
+    cy.wait('@saveData').then((xhr) => {
+        expect(xhr.response).to.have.property('statusCode', 200);
+    });
+}
+
+export default {
+    changeColorScheme: schema => changeColorScheme(schema)
+}
